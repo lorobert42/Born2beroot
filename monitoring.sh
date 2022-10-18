78389c9a7321214fe2575c831c5c2c09c92c4245
lorobert <user@PC-Loic>
2022-10-18 19:31:36 +0200

    Conf

---
+++ monitoring.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+wall $'#Architecture:' `uname -a` \
+$'\n#CPU physical:' `cat /proc/cpuinfo | grep 'physical id' | uniq | wc -l` \
+$'\n#vCPU:' `cat /proc/cpuinfo | grep 'processor' | wc -l` \
+$'\n#Memory Usage:' `free -m | grep 'Mem:' | awk '{printf("%d/%dMB (%.2f%%)\n", $3, $4, $3 / $4 * 100)}'` \
+$'\n#Disk Usage:' `df --total | grep 'total' | awk '{printf("%.1f/%.1fGb (%.1f%%)", $3 / (1024*1024), $4 / (1024*1024), $5)}'` \
+$'\n#CPU load:' `top -bn1 | grep '%Cpu' | cut -d ',' -f 4 | awk '{printf("%.1f%%", 100 - $1)}'` \
+$'\n#Last boot:' `who -b | awk '{print $3, $4}'` \
+$'\n#LVM use:' `lsblk | grep 'lvm' -m 1 | awk '{if($0){print "yes"} else {print "no"}}'` \
+$'\n#TCP Connections:' `ss -at | grep 'ESTAB' | wc -l` 'ESTABLISHED' \
+$'\n#User log:' `who | cut -d ' ' -f 1 | sort -u | wc -l` \
+$'\n#Network: IP' `hostname -I | awk '{print $1}'` `ip a | grep 'link/ether' | awk '{printf("(%s)", $2)}'` \
+$'\n#Sudo:' `grep 'COMMAND' /var/log/sudo/sudo.log | wc -l` 'cmd'
